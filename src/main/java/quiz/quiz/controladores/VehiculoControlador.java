0196b5dd0ef7849f12b7ae034041ab5010aa3421
LauraaCa <lauracastanedar962@gmail.com>
2025-03-28 21:06:56 -0500

    feature(bkend) crear vahiculo

--- src/main/java/quiz/quiz/controladores/VehiculoControlador.java
+++ src/main/java/quiz/quiz/controladores/VehiculoControlador.java
@@ -1,4 +1,22 @@
 package quiz.quiz.controladores;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import quiz.quiz.dto.VehiculoDTO;
+import quiz.quiz.persistencia.entidades.Vehiculo;
+import quiz.quiz.servicios.VehiculoServicio;
+
+@RestController
+@AllArgsConstructor
+@CrossOrigin(origins = "*")
+@RequestMapping("/vehiculos")
 public class VehiculoControlador {
+    VehiculoServicio vehiculoServicio;
+
+    @PostMapping("/")
+    public VehiculoDTO crear(@RequestBody VehiculoDTO vehiculo){
+        return vehiculoServicio.crear(vehiculo);
+    }
+
+
 }
--- src/main/java/quiz/quiz/persistencia/entidades/Vehiculo.java
+++ src/main/java/quiz/quiz/persistencia/entidades/Vehiculo.java
@@ -1,4 +1,30 @@
 package quiz.quiz.persistencia.entidades;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "vehiculos")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
 public class Vehiculo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(unique=true)
+    private String placa;
+    private String color;
+    private Long modelo;
+    private LocalDateTime lanzamiento;
+    private String cajaTransmision;
+
 }
+
+
